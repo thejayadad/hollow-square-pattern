c5ce161ade9dc2f3bd80f1d6071dc9e0111299d3
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2022-08-16 12:56:35 -0400

    update

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+let row = 5;
+
+for (let i = 1; i <= row; i++){
+    for (let j = 1; j<= row; j++){
+        if ( i == 1 || j == 1 || i == row || j == row){
+            document.write("x")
+        } else {
+            document.write(" &nbsp;")
+        }
+    }
+    document.write("<br/>")
+}
